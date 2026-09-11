b33f7ca070bd951e76e431ca0b457b923535168f
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-09-11 10:36:04 -0700

    Create : Reservation models

--- core/reservation/admin.py
+++ core/reservation/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ReservationModel, ReservationConfigModel
 
 # Register your models here.
+
+@admin.register(ReservationModel)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "phone_number", "status"]
+    search_fields = ["last_name"]
+    list_filter = ["status"]
+
+@admin.register(ReservationConfigModel)
+class ReservationConfigAdmin(admin.ModelAdmin):
+    list_display = ["day_of_week", "max_limit_reserve", "expired_date"]
+    search_fields = ["day_of_week"]
--- core/reservation/models.py
+++ core/reservation/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.utils import timezone
+from .validators import validate_iranian_cellphone_number
 # Create your models here.
+
+
+class ReservationModel(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15 ,validators=[validate_iranian_cellphone_number])
+    subject = models.CharField(max_length=1000)
+    reservation_config = models.ForeignKey("ReservationConfigModel", on_delete=models.PROTECT)
+    class ReservationStatusModel(models.TextChoices):
+        PENDING = "pending", "Pending"
+        COMPLETED = "completed", "Completed"
+        CANCELLED = "cancelled", "Cancelled"
+
+    status = models.CharField(default=ReservationStatusModel.PENDING, choices=ReservationStatusModel.choices, max_length=20)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class ReservationConfigModel(models.Model):
+    day_of_week = models.CharField(max_length=100, null=True, blank=True)
+    max_limit_reserve = models.PositiveIntegerField(default=0)
+    expired_date = models.DateTimeField(default=timezone.now)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.day_of_week
+
+    class Meta:
+        ordering = ["-created_date"]
