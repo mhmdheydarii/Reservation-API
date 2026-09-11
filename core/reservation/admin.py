6779668fcec4b30508c3a9dfdd672f9712d2d2c9
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-09-11 14:24:53 -0700

    Update : admin.py

--- core/reservation/admin.py
+++ core/reservation/admin.py
@@ -5,11 +5,11 @@ from .models import ReservationModel, ReservationConfigModel
 
 @admin.register(ReservationModel)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "phone_number", "status"]
+    list_display = ["id", "first_name", "last_name", "phone_number", "status"]
     search_fields = ["last_name"]
     list_filter = ["status"]
 
 @admin.register(ReservationConfigModel)
 class ReservationConfigAdmin(admin.ModelAdmin):
-    list_display = ["day_of_week", "max_limit_reserve", "expired_date"]
+    list_display = ["id", "day_of_week", "max_limit_reserve", "expired_date"]
     search_fields = ["day_of_week"]
