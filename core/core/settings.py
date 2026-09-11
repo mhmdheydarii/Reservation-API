bfa1379114d9c8f571ddff02abf551fc8c839d4f
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-09-11 09:56:31 -0700

    StartApp : Reservation

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'drf_yasg',
     "rest_framework",
+
+    # Apps
+    "reservation",
 ]
 
 MIDDLEWARE = [
--- core/core/urls.py
+++ core/core/urls.py
@@ -38,6 +38,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reservation/', include("reservation.urls")),
+    
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
